a726d92e32e065c906a93c3e2fc2975d69436201
allmightdude <sabzi_f@yahoo.com>
2023-01-11 17:44:38 +0330

    Edit Profile

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="profile mt-4">
     <h1>Profile Page</h1>
-    <form @submit.prevent="onLogin">
+    <form @submit.prevent="onUpdate">
       <div class="form__control">
         <label for="name">Name</label>
-        <input type="text" class="form__input" v-model="name" :placeholder="$auth.state.user.name" />
+        <input
+          type="text"
+          class="form__input"
+          v-model="name"
+          :placeholder="$auth.state.user.name"
+        />
       </div>
 
       <div class="form__control">
         <label for="email">E-mail</label>
-        <input type="email" class="form__input" v-model="email" :placeholder="$auth.state.user.email" />
+        <input
+          type="email"
+          class="form__input"
+          v-model="email"
+          :placeholder="$auth.state.user.email"
+        />
       </div>
 
       <div class="form__control">
@@ -17,9 +27,7 @@
         <input type="password" class="form__input" v-model="password" />
       </div>
 
-        <base-button type="submit" class="mt-4">
-          Update Profile
-        </base-button>
+      <base-button type="submit" class="mt-4"> Update Profile </base-button>
     </form>
   </div>
 </template>
@@ -27,13 +35,34 @@
 <script>
 export default {
 
+  data() {
+    return {
+      name : "",
+      email : "",
+      password : ""
+    }
+  },
+  methods: {
+    async onUpdate(){
+      try {
+        let data = {
+          name : this.name,
+          email : this.email,
+          password : this.password
+        }
+        let res = await this.$axios.$put("/api/auth/user" , data);
+        console.log();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
 };
 </script>
 
-
 <style lang="scss" scoped>
-.profile{
-    max-width: 30rem;
-    margin: 3rem auto;
+.profile {
+  max-width: 30rem;
+  margin: 3rem auto;
 }
 </style>
